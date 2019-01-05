e61f42384e55472b662e16671a3681873df5ba65
septianaana <45724156+septianaana@users.noreply.github.com>
2019-01-05 11:48:56 +0700

    Add files via upload

---
+++ konversi suhu.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    float C,F,K,R;
+    cout<<"program konversi suhu"<<endl;
+    cout<<"versi faqih muhammad"<<endl;
+    cout<<"masukan nilai celcius = ";
+    cin>>C;
+    F=C*1.8 + 32;
+    K=C+273.15;
+    R=C*0.8;
+    cout<<"nilai fahrenheit = "<<F<<endl<<"nilai kelvin = "<<K<<endl<<"nilai reamur = "<<R;
+
+    std::cout<<" \n";
+    std::cout<<"nama  : muhammad yusuf alfaqih \n";
+    std::cout<<"nim   : 311810239";
+    return 0;
+}
